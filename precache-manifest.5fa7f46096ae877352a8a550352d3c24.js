f9bf11663b442c99344f7a5aa5acbf95a4b1a56c
Maryna Zinchenko <pandoz16@gmail.com>
2021-01-15 17:56:39 +0200

    Updates

---
+++ precache-manifest.5fa7f46096ae877352a8a550352d3c24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "971131d357f6300fd16664186163074d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5da98142d1831a1f5dd6",
+    "url": "./static/css/main.e98965bc.chunk.css"
+  },
+  {
+    "revision": "4e3c0e2e69ce97e4426e",
+    "url": "./static/js/2.c63ab0fc.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.c63ab0fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da98142d1831a1f5dd6",
+    "url": "./static/js/main.e9d7d256.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
